7a88cc1230d9900a4ea5d282afa68e254d24faa1
wuzhaohui <thwuzhaohui@163.com>
2016-04-09 17:40:42 +0800

    finish BruteForceSearch for codecraft

---
+++ codecraft/SDK-gcc-1.0/future_net/graph.h
@@ -0,0 +1,94 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <vector>
+#include <map>
+#include <stdlib.h>
+
+/*
+#include<limits>
+const int INT_INF = std::numeric_limits<int>::max();
+*/
+#define  MAX_VERT_NUM 601
+#define  MAX_INT_NUM 0x7fffffff
+struct gNode{
+	int edgeId;
+	int vert;
+	int weight;
+	gNode *next;
+	gNode(int v, int w, gNode *t){
+		vert = v;
+		weight = w;
+		next = t;
+	}
+	gNode(int id, int v, int w, gNode *t){
+		edgeId = id;
+		vert = v;
+		weight = w;
+		next = t;
+	}
+};
+typedef gNode* gLink;
+class Graph{
+	int vertCount;
+	int edgeCount;
+	bool digraph;
+	int minCost;
+	bool debug;
+	std::vector<bool> visited;
+	std::vector<int> bestPath;
+	std::vector<int> bestEdgePath;
+	std::map<int ,int > vertIdMap;// source Id -> Graph Id
+	//int edgeId[MAX_VERT_NUM][MAX_VERT_NUM]; // store the map between edge id and id; do not use at now.
+	std::vector<gLink> adj;
+
+	std::vector<int> passVert;
+public:
+	Graph():
+		vertCount(0), edgeCount(0), digraph(false){}
+	Graph(int V, bool dig = false);
+	Graph(bool dig = false) { digraph = dig; vertCount = 0; edgeCount =0; minCost = MAX_INT_NUM;}
+	int get_vertexCount(){ return vertCount;}
+	void set_vertexCount(int vert){ vertCount = vert;}
+	int get_edgeCount(){ return edgeCount;}
+	void set_edgeCount(int edge){ edgeCount = edge;}
+	int get_digraph(){ return digraph;}
+	int get_visited(int vert){ return visited[vert];}
+	void set_visited(int vert){ visited[vert] = true;}
+	gLink get_adj(int vert){ return adj[vert];};
+	void initAdj();
+	void set_debug(bool de){debug = de;}
+	bool get_debug() {return debug;}
+
+	void initPassVert(std::vector<int> v);
+
+	gLink FindFirstAdj(int vert) { return adj[vert]->next;}
+	gLink FindNextAdj(int v, int w);
+
+	int FindEdgeId(int v, int w);
+
+	std::vector<int> getBestEdgePath();
+
+	void insert(int v, int w, int weight = 1);
+	void insert(int id, int v, int w, int weight = 1);
+	void insert(int edge[4]);
+
+	void insertVertId(int num);
+	int getVertId(int num){return vertIdMap[num];};
+
+	bool CheckEdge(int v, int w) ;
+
+	void clear_visited(){ visited.assign(vertCount, false);}
+
+	void DFS(int vert = 0);
+	void BFS(int vert = 0);
+	std::vector<int> TopoSort();
+
+	void BruteForceSearch();
+
+	void printAllEdge();
+	void printPassVert();
+	void printBestEdgePath();
+
+};
+
+#endif
---
+++ codecraft/SDK-gcc-1.0/future_net/graph_future.cpp
@@ -0,0 +1,71 @@
+#include "graph.h"
+#include <queue>
+#include <algorithm>
+#include <iostream>
+struct Path{
+    std::vector<int> path;
+    unsigned int passCnt;
+    int cost;
+    Path(){passCnt = 0; cost = 0;}
+    Path(const Path & pa){
+        path = pa.path;
+        passCnt = pa.passCnt;
+        cost = pa.cost;
+    }
+};
+
+void printVec(std::vector<int> v);
+void printPath(Path & pa);
+
+void Graph::BruteForceSearch(){
+    std::queue<Path> que;
+    Path first;
+    first.path.push_back(passVert[0]);
+    first.passCnt = 1;
+    que.push(first);
+    while(!que.empty()){
+        Path pa = que.front();
+        //printVec(pa.path);
+        //printPath(pa);
+        for(gLink g = adj[pa.path.back()]->next; g!=NULL; g=g->next){
+            Path newPa(pa);
+            if(std::find(newPa.path.begin(), newPa.path.end(), g->vert) != newPa.path.end())
+                continue; // Loop
+            if(g->vert == passVert[1] && newPa.passCnt != passVert.size()-1)
+                continue; // go to end without enough pass vert.
+            if(std::find(passVert.begin(), passVert.end(), g->vert) != passVert.end())
+                newPa.passCnt++;
+            newPa.cost = newPa.cost + g->weight;
+            newPa.path.push_back(g->vert);
+            //std::cout<<"passcnt: "<<newPa.passCnt<<"\n";
+            if(newPa.passCnt == passVert.size()){
+                if(newPa.cost < minCost){
+                    if(debug){
+                        std::cout<<"find better path:\n";
+                        printPath(newPa);
+                    }
+                    minCost = newPa.cost;
+                    bestPath = newPa.path;
+                }
+                continue; // if all pass, don't find more.
+            }
+            que.push(newPa);
+        }
+        que.pop();
+    }
+}
+
+void printVec(std::vector<int> v){
+    for(unsigned int i =0; i< v.size(); i++)
+        std::cout<<v[i]<<" ";
+    std::cout<<"\n";
+}
+
+void printPath(Path & pa){
+    std::cout<<"Path: ";
+    for(unsigned int i =0; i< pa.path.size(); i++)
+        std::cout<<pa.path[i]<<" ";
+    std::cout<<"\n";
+    std::cout<<"cost: "<<pa.cost<<"\n";
+    std::cout<<"passCnt: "<<pa.passCnt<<"\n\n";
+}
