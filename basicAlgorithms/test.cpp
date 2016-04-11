25f8e1e55224e3f7522f8ba1fa5587530f0daf54
wuzhaohui <thwuzhaohui@163.com>
2016-04-11 22:57:54 +0800

    update test

--- basicAlgorithms/test.cpp
+++ basicAlgorithms/test.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<cstring>
 #include<string>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 void test(int a[]){
 	int *b =new int[3];
@@ -9,6 +12,17 @@ void test(int a[]){
 }
 int main(){
 	int a[] = {1,2,3};
+	double dd = 3.5;
+	srand (time(NULL));
+	int N =1000,n=0;
+	while(N--) {
+		double temp =rand() / (double)(RAND_MAX);
+		if(temp<0.1)
+			n++;
+	}
+	cout<<"N:"<<n<<endl;
+	//double ran = rand() / (double)(RAND_MAX);
+	cout<<1/dd<<endl;
 	test(a);
 	for(int i=0;i<3;i++) cout<<a[i]<<" ";
 	cout<<endl;
